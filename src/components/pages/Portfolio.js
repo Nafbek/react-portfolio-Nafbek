0514cccd4e53f78bc6ac065807b53eb48e610c16
Bekam <mspbekan@gmail.com>
2023-07-31 23:00:50 -0500

    custom css modified and project's  anchor wrapper removed

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -50,176 +50,164 @@ export default function Projects() {
         <div className="projects-container">
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-2">
             <div className="col-md mb-3">
-              <a className="project-link-container" href="#">
-                <div
-                  className="p-3 project-box project-box-height"
-                  style={style.project1}
+              <div
+                className="p-3 project-box project-box-height"
+                style={style.project1}
+              >
+                <a
+                  className="name-emoji-link"
+                  href="https://jennastrom.github.io/Beer-and-a-show/"
                 >
-                  <a
-                    className="name-emoji-link"
-                    href="https://jennastrom.github.io/Beer-and-a-show/"
-                  >
-                    Beer and A Show
-                  </a>
-                  <a
-                    className="name-emoji-link"
-                    href="https://github.com/JennaStrom/Beer-and-a-show"
-                  >
-                    <img
-                      src={
-                        process.env.PUBLIC_URL + "images/github-mark-white.png"
-                      }
-                      style={style.iconsize}
-                    />
-                  </a>
-                  <p className="links-paragraph" style={style.description}>
-                    Frontend
-                  </p>
-                </div>
-              </a>
+                  Beer and A Show
+                </a>
+                <a
+                  className="name-emoji-link"
+                  href="https://github.com/JennaStrom/Beer-and-a-show"
+                >
+                  <img
+                    src={
+                      process.env.PUBLIC_URL + "/images/github-mark-white.png"
+                    }
+                    style={style.iconsize}
+                  />
+                </a>
+                <p className="links-paragraph" style={style.description}>
+                  Frontend
+                </p>
+              </div>
             </div>
 
             <div className="col-md mb-3">
-              <a className="project-link-container" href="#">
-                <div
-                  className="p-3 project-box project-box-height"
-                  style={style.project2}
+              <div
+                className="p-3 project-box project-box-height"
+                style={style.project2}
+              >
+                <a
+                  className="name-emoji-link"
+                  href="https://hidden-lake-38607-a47be9049bc1.herokuapp.com/"
                 >
-                  <a
-                    className="name-emoji-link"
-                    href="https://hidden-lake-38607-a47be9049bc1.herokuapp.com/"
-                  >
-                    Nutrition Ninja
-                  </a>
-                  <a
-                    className="name-emoji-link"
-                    href="https://github.com/JennaStrom/nutrition-ninja"
-                  >
-                    <img
-                      src={
-                        process.env.PUBLIC_URL + "images/github-mark-white.png"
-                      }
-                      style={style.iconsize}
-                    />
-                  </a>
-                  <p className="links-paragraph" style={style.description}>
-                    Full Stack
-                  </p>
-                </div>
-              </a>
+                  Nutrition Ninja
+                </a>
+                <a
+                  className="name-emoji-link"
+                  href="https://github.com/JennaStrom/nutrition-ninja"
+                >
+                  <img
+                    src={
+                      process.env.PUBLIC_URL + "/images/github-mark-white.png"
+                    }
+                    style={style.iconsize}
+                  />
+                </a>
+                <p className="links-paragraph" style={style.description}>
+                  Full Stack
+                </p>
+              </div>
             </div>
 
             <div className="col-md mb-3">
-              <a className="project-link-container" href="#">
-                <div
-                  className="p-3 project-box project-box-height"
-                  style={style.project3}
+              <div
+                className="p-3 project-box project-box-height"
+                style={style.project3}
+              >
+                <a
+                  className="name-emoji-link"
+                  href="https://nafbek.github.io/weather-dashboard-5days-forecast/"
                 >
-                  <a
-                    className="name-emoji-link"
-                    href="https://nafbek.github.io/weather-dashboard-5days-forecast/"
-                  >
-                    Weather Dashboard
-                  </a>
-                  <a
-                    className="name-emoji-link"
-                    href="https://github.com/Nafbek/weather-dashboard-5days-forecast"
-                  >
-                    <img
-                      src={
-                        process.env.PUBLIC_URL + "images/github-mark-white.png"
-                      }
-                      style={style.iconsize}
-                    />
-                  </a>
-                  <p className="links-paragraph" style={style.description}>
-                    Frontend
-                  </p>
-                </div>
-              </a>
+                  Weather Dashboard
+                </a>
+                <a
+                  className="name-emoji-link"
+                  href="https://github.com/Nafbek/weather-dashboard-5days-forecast"
+                >
+                  <img
+                    src={
+                      process.env.PUBLIC_URL + "/images/github-mark-white.png"
+                    }
+                    style={style.iconsize}
+                  />
+                </a>
+                <p className="links-paragraph" style={style.description}>
+                  Frontend
+                </p>
+              </div>
             </div>
 
             <div className="col-md mb-3">
-              <a className="project-link-container" href="#">
-                <div
-                  className="p-3 project-box project-box-height"
-                  style={style.project4}
+              <div
+                className="p-3 project-box project-box-height"
+                style={style.project4}
+              >
+                <a
+                  className="name-emoji-link"
+                  href="https://nafbek.github.io/password-generator-challenge3/"
                 >
-                  <a
-                    className="name-emoji-link"
-                    href="https://nafbek.github.io/password-generator-challenge3/"
-                  >
-                    Password Generator
-                  </a>
-                  <a
-                    className="name-emoji-link"
-                    href="https://github.com/Nafbek/password-generator-challenge3/settings/pages"
-                  >
-                    <img
-                      src={
-                        process.env.PUBLIC_URL + "images/github-mark-white.png"
-                      }
-                      style={style.iconsize}
-                    />
-                  </a>
-                  <p className="links-paragraph" style={style.description}>
-                    Frontend
-                  </p>
-                </div>
-              </a>
+                  Password Generator
+                </a>
+                <a
+                  className="name-emoji-link"
+                  href="https://github.com/Nafbek/password-generator-challenge3/settings/pages"
+                >
+                  <img
+                    src={
+                      process.env.PUBLIC_URL + "/images/github-mark-white.png"
+                    }
+                    style={style.iconsize}
+                  />
+                </a>
+                <p className="links-paragraph" style={style.description}>
+                  Frontend
+                </p>
+              </div>
             </div>
             <div className="col-md mb-3">
-              <a className="project-link-container" href="#">
-                <div
-                  className="p-3 project-box project-box-height"
-                  style={style.project5}
+              <div
+                className="p-3 project-box project-box-height"
+                style={style.project5}
+              >
+                <a
+                  className="name-emoji-link"
+                  href="https://nafbek.github.io/work-day-scheduler/"
                 >
-                  <a
-                    className="name-emoji-link"
-                    href="https://nafbek.github.io/work-day-scheduler/"
-                  >
-                    Work Day Scheduler
-                  </a>
-                  <a
-                    className="name-emoji-link"
-                    href="https://github.com/Nafbek/work-day-scheduler"
-                  >
-                    <img
-                      src={
-                        process.env.PUBLIC_URL + "images/github-mark-white.png"
-                      }
-                      style={style.iconsize}
-                    />
-                  </a>
-                  <p className="links-paragraph" style={style.description}>
-                    Frontend
-                  </p>
-                </div>
-              </a>
+                  Work Day Scheduler
+                </a>
+                <a
+                  className="name-emoji-link"
+                  href="https://github.com/Nafbek/work-day-scheduler"
+                >
+                  <img
+                    src={
+                      process.env.PUBLIC_URL + "/images/github-mark-white.png"
+                    }
+                    style={style.iconsize}
+                  />
+                </a>
+                <p className="links-paragraph" style={style.description}>
+                  Frontend
+                </p>
+              </div>
             </div>
 
             <div className="col-md mb-3">
-              <a className="project-link-container" href="#">
-                <div
-                  className="p-3 project-box project-box-height"
-                  style={style.project6}
-                >
-                  <a className="name-emoji-link" href="#">
-                    FlySetter
-                  </a>
-                  <a className="name-emoji-link" href="#">
-                    <img
-                      src={
-                        process.env.PUBLIC_URL + "images/github-mark-white.png"
-                      }
-                      style={style.iconsize}
-                    />
-                  </a>
-                  <p className="links-paragraph" style={style.description}>
-                    MERN Stack
-                  </p>
-                </div>
-              </a>
+              <div
+                className="p-3 project-box project-box-height"
+                style={style.project6}
+              >
+                <a className="name-emoji-link" href="#">
+                  FlySetter
+                </a>
+                <a className="name-emoji-link" href="#">
+                  <img
+                    src={
+                      process.env.PUBLIC_URL + "/images/github-mark-white.png"
+                    }
+                    style={style.iconsize}
+                  />
+                </a>
+                <p className="links-paragraph" style={style.description}>
+                  MERN Stack
+                </p>
+              </div>
             </div>
           </div>
         </div>
